898d723a0011e15bba42cabcc5d6c814c966b853
Smarter-Poker <admin@smarter.poker>
2026-01-18 20:03:29 -0600

    US Event Coverage Master: Fail-closed ingestion pipeline with PokerAtlas/Hendon integration

---
+++ src/ingest_us_events/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Index export for ingest_us_events module
+ */
+
+export * from './types';
+export * from './pokeratlas_series_directory';
+export * from './pokeratlas_series_page';
+export * from './pokeratlas_venues';
+export * from './hendon_festivals';
+export * from './normalize';
+export * from './merge';
