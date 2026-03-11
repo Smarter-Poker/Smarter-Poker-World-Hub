df70fadb56ffa8f48caaa152d77523d3dd771f95
Smarter-Poker <admin@smarter.poker>
2026-03-10 19:18:24 -0500

    chore(telemetry): saturate Club Arena pages with useTrainingBus

--- pages/hub/club-arena/player-stats.js
+++ pages/hub/club-arena/player-stats.js
@@ -2,8 +2,11 @@
 import SEOHead from '../../../src/components/seo/SEOHead';
 import UniversalHeader from '../../../src/components/ui/UniversalHeader';
 import ClubArenaEmbed from '../../../src/components/club-arena/ClubArenaEmbed';
+import { useTrainingBus } from '../../../src/hooks/useTrainingBus';
 
 export default function ClubArenaPlayerStatsPage() {
+    useTrainingBus();
+
     return (
         <>
             <SEOHead title="Player Stats | Smarter.Poker" />
--- pages/hub/club-arena/union-games.js
+++ pages/hub/club-arena/union-games.js
@@ -2,8 +2,11 @@
 import SEOHead from '../../../src/components/seo/SEOHead';
 import UniversalHeader from '../../../src/components/ui/UniversalHeader';
 import ClubArenaEmbed from '../../../src/components/club-arena/ClubArenaEmbed';
+import { useTrainingBus } from '../../../src/hooks/useTrainingBus';
 
 export default function ClubArenaUnionGamesPage() {
+    useTrainingBus();
+
     return (
         <>
             <SEOHead title="Union Games | Smarter.Poker" />
