48755467eec56ca6afdf38a51773a5437bc887a0
Dan <danbek4545@gmail.com>
2026-04-20 15:26:47 -0500

    fix(autofix): remove e2e test files, size guard 500-char threshold

--- pages/autofix-e2e-test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// Intentional unused import to trigger a build warning/error
-import { NotReal } from '../src/lib/doesNotExist';
-
-export default function AutofixE2ETest() {
-  return <div>Autofix E2E Test Page</div>;
-}
