fb594c585e63b3ee147c8a75a83b6eaa782cf669
Smarter-Poker <admin@smarter.poker>
2026-04-20 17:10:05 -0500

    [autofix] fix build error in pages/proof-test.js (attempt 1)
    Auto-generated by deploy-monitor. Original error:
    Error: Command "npm run build" exited with 1
    > Build failed because of webpack errors
    https://nextjs.org/docs/messages/module-not-found
    Module not found: Can't resolve '../src/components/INTENTIONALLY

--- pages/proof-test.js
+++ pages/proof-test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FakeComponent } from '../src/components/INTENTIONALLY_BROKEN_PROOF';
 
 export default function ProofTest() {
-  return <div><FakeComponent /></div>;
+  return <div>Proof Test</div>;
 }
