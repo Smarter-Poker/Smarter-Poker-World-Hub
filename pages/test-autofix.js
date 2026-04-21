071f0d58257af22cd6e39f3cf04cb096576ed22f
Dan <danbek4545@gmail.com>
2026-04-20 20:05:18 -0500

    chore(test): remove intentional live-test broken file [cleanup]

--- pages/test-autofix.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { nonExistentThing } from '../lib/does-not-exist-for-autofix-test';
-
-export default function TestAutofix() {
-  return <div>{nonExistentThing()}</div>;
-}
