8f552e01dfd3a47feabbdb61eb5be8acc3c2e38a
Smarter-Poker <254329056+Smarter-Poker@users.noreply.github.com>
2026-06-21 12:25:19 -0500

    style: Finalize best-bets and standings UI with shared components

--- pages/hub/MLB-ANALYTICS/best-bets.tsx
+++ pages/hub/MLB-ANALYTICS/best-bets.tsx
@@ -23,9 +23,7 @@ import MlbSubNav from '../../../src/components/ui/MlbSubNav';
 import BottomNavBar from '../../../src/components/ui/BottomNavBar';
 import SEOHead from '../../../src/components/seo/SEOHead';
 import { logError } from '@/utils/logger';
-import { detectBetCategory } from '../../../src/lib/mlb_data';
-import { formatOdds, formatWinPct, toTitleCase, formatBetType } from '../../../src/lib/formatters';
-import { getPlayerImageUrl, getTeamLogoUrl, MLB_TEAM_IDS } from '../../../src/lib/mlb_images';
+
 import { ScoringGuideModal } from '../../../src/components/mlb/ScoringGuideModal';
 import MlbPremiumGate from '../../../src/components/mlb/MlbPremiumGate';
 import MetalFrame from '../../../src/components/ui/MetalFrame';
