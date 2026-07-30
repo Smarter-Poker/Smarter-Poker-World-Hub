8df52205ad771b1c96bb6ec231e8a073c7711a08
Smarter-Poker <254329056+Smarter-Poker@users.noreply.github.com>
2026-07-29 21:21:22 -0500

    fix(api): restore depth-correct serverAuth imports + avatar system fixes (batch 3)

--- pages/api/training/save-progress.js
+++ pages/api/training/save-progress.js
@@ -1,3 +1,4 @@
+import { getServerUserWithFallback } from '../../../src/lib/serverAuth';
 /**
  * API: Save Training Progress
  * ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
