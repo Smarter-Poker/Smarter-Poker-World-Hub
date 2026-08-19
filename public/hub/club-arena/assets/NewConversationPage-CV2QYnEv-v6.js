92bf1536b0a264c54afa0279f93d1cf4e6676b53
Smarter-Poker <254329056+Smarter-Poker@users.noreply.github.com>
2026-08-19 13:02:30 -0500

    ui(club-arena): final bottom cards perfectly matched to WorldHub frame style without bottom bulk

---
+++ public/hub/club-arena/assets/NewConversationPage-CV2QYnEv-v6.js
@@ -0,0 +1,2 @@
+import{u as a,r as t}from"./vendor-react-B6_Sci7B-v6.js";function s(){const e=a();return t.useEffect(()=>{e("/messages",{replace:!0})},[e]),null}export{s as default};
+//# sourceMappingURL=NewConversationPage-CV2QYnEv-v6.js.map
